3efd9e1fe7ed9a3a0a810cadfdf1e92faf198684
Ryan You <rjcsebkk@gmail.com>
2024-03-10 16:09:43 -0400

    Final commit

---
+++ exercise-4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var customer_1 = require("./customer");
+var customer = new customer_1.Customer("Ryan", "You", 39);
+customer.greeter();
+customer.GetAge();
---
+++ exercise-4/main.ts
@@ -0,0 +1,5 @@
+import { Customer } from './customer';
+
+let customer = new Customer("Ryan", "You", 39);
+customer.greeter();
+customer.GetAge();
